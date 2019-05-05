30751f3ce3d2220a7b70d0128d7f7ad9037f91a7
Carlos R. Zúñiga <carlosrozuma@gmail.com>
2019-05-05 14:03:10 -0700

    fixed file upload php example

--- PHP/upload/upload.php
+++ PHP/upload/upload.php
@@ -1,25 +1,33 @@
 <?php
 
-    if(isset($_FILES['image'])){
-        $errors= array();
-        $file_name = $_FILES['image']['name'];
-        $file_size =$_FILES['image']['size'];
-        $file_tmp =$_FILES['image']['tmp_name'];
-        $file_type=$_FILES['image']['type'];
-        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+    if( isset($_FILES['image']) ){
+        $errors     = array();
+        $imgPath    = 'images/';
         
-        $expensions= array("jpeg","jpg","png");
+        // file info
+        $file_name  = $_FILES['image']['name'];
+        $file_size  = $_FILES['image']['size'];
+        $file_tmp   = $_FILES['image']['tmp_name'];
+        $file_type  = $_FILES['image']['type'];
         
-        if(in_array($file_ext,$expensions)=== false){
-            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+        // File extension
+        $file_ext   = explode('.', $file_name);
+        $file_ext   = strtolower(end($file_ext));
+
+        $extensions = array("jpeg","jpg","png");
+        
+        if( in_array( $file_ext, $extensions ) === false ){
+            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
         }
         
-        if($file_size > 2097152){
-            $errors[]='File size must be excately 2 MB';
+        if( $file_size > 2097152 ){
+            $errors[] ='File size must be excately 2 MB';
         }
         
-        if(empty($errors)==true){
-            move_uploaded_file($file_tmp,"images/".$file_name);
+        if( empty($errors) == true){
+            if (!file_exists($imgPath)) mkdir( $imgPath, 0777, true);
+
+            move_uploaded_file( $file_tmp, $imgPath.$file_name );
             echo "Success";
         }else{
             print_r($errors);
